2c4f194635ad76808a319cb5e514bfa2dc890e35
JZhg02 <94388221+JZhg02@users.noreply.github.com>
2022-09-08 20:22:28 -0400

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,40 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String []args){
+
+        int lineNumber = 4;
+
+        int arrowCounter = 0;
+
+        try{
+            // File
+            File file = new File("DNA_data.txt");
+            // Create a file reader
+            FileReader fr = new FileReader(file);
+            // Create a buffered reader
+            BufferedReader br = new BufferedReader(fr);
+
+            String line;
+
+            // While we are not at the end of the file
+            while((line = br.readLine()) != null){
+                // If the line is not empty
+                if(!line.isEmpty()){
+                    // arrow = first character of the line
+                    String arrow = line.charAt(0)+"";
+                    if(arrow.equals(">")){
+                        arrowCounter++;
+                        if(arrowCounter == lineNumber){
+                            System.out.println(br.readLine());
+                        }
+                    }
+                }
+            }
+            fr.close();
+        }
+        catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+}
